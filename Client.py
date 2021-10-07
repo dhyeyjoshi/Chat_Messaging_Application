78d9f352c3db1ea10001d553188b0b81f6b965a1
Dhyey <dhyey1974@gmail.com>
2021-10-08 00:38:47 +0530

    Version:1

---
+++ Client.py
@@ -0,0 +1,98 @@
+import socket
+import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+HOST = '127.0.0.1' #assign public ip here if client is on internet and private ip in server
+PORT = 3000
+
+class Client:
+
+    def __init__(self, host, port):
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host,port))
+
+        msg = tkinter.Tk()
+        msg.withdraw()
+        msg.configure(bg='blue')
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
+
+        self.gui_done = False
+        self.running = True
+
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
+
+        gui_thread.start()
+        receive_thread.start()
+
+    def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.title("Chat Karo Dil Se - Author Dhyey Joshi")
+        self.win.configure(bg="lightblue")
+
+        self.chat_label = tkinter.Label(self.win, text="Chat:",bg="lightblue")
+        self.chat_label.config(font=("Arial",12))
+        self.chat_label.pack(padx=20,pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20,pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.win, text="Message: ",bg="lightblue")
+        self.msg_label.config(font=("Arial",12))
+        self.msg_label.pack(padx=20,pady=5)       
+
+        self.input_area = tkinter.Text(self.win, height=5)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial",12))
+        self.send_button.pack(padx=20,pady=5)
+
+        self.gui_done = True
+
+        self.win.protocol("WM_DELETE_WINDOW",self.stop)
+        
+        self.win.mainloop()
+
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0','end')}"
+        self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0','end')
+
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+
+    def receive(self):
+        while self.running:
+            try:
+                message = self.sock.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode('utf-8'))
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state = 'disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.sock.close()
+                break
+
+client = Client(HOST,PORT)
+                
+
+
+                 
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Chat_Message_Application
+# PYTHON_CHAT_MESSAGE_APP
+
+<h2>Chat Messaging App: Using Concepts Of Socket Programming & Multi Threading</h2>
+<h3>Best Usecase of Server - Client Model</h3>
+
+1. Start/Run the Server File first then Run Client File.
+2. Give your Nickname.
+3. You Can Create unlimited clients by runing client file again & again.
+4. Advancement can be done in this project by adding Using Video Stream with Audio/Face Detection & Recognition/Create End to End Application ~ Deploy on Clouds like AWs,Azure,Gcp,Heroku etc.. 
+
+## Happy coding!
